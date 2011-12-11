af6c9bfaa4f4f2eac5dc4fbc262a81640cd7dafc
Marcel Beck <fo3nik5@gmail.com>
2011-12-11 20:15:40 +0100

    Initial Split JS

---
+++ var/www/openmediavault/js/omv/module/admin/greyhole.js
@@ -0,0 +1,8 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mbeck
+ * Date: 11.12.11
+ * Time: 20:11
+ * To change this template use File | Settings | File Templates.
+ */
+// require("js/omv/module/greyhole/admin.js")
