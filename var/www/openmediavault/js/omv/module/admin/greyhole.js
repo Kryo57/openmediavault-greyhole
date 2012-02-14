a26bea1d6c9baf74b6854aa047b4476bb64e81bb
Marcel Beck <fo3nik5@gmail.com>
2012-02-14 23:58:30 +0100

    Log - Show time, as find in the logs

--- var/www/openmediavault/js/omv/module/admin/greyhole.js
+++ var/www/openmediavault/js/omv/module/admin/greyhole.js
@@ -5,4 +5,48 @@
  * Time: 20:11
  * To change this template use File | Settings | File Templates.
  */
+
 // require("js/omv/module/greyhole/admin.js")
+// require("js/omv/util/Format.js")
+
+OMV.Module.Diagnostics.LogPlugin.Greyhole = function (config) {
+	var initialConfig = {
+		title    :"Greyhole",
+		stateId  :"a4150311-5e3a-4693-8381-933088a9f98f",
+		columns  :[
+			{
+				header   :"Date & Time",
+				sortable :true,
+				dataIndex:"date",
+				id       :"date",
+				width    :35
+			},
+			{
+				header   :"Component",
+				sortable :true,
+				dataIndex:"component",
+				width    :20
+			},
+			{
+				header   :"Event",
+				sortable :true,
+				dataIndex:"event",
+				id       :"event"
+			}
+		],
+		rpcArgs  :"greyhole",
+		rpcFields:[
+			{ name:"date" },
+			{ name:"component" },
+			{ name:"event" }
+		]
+	};
+	Ext.apply(initialConfig, config);
+	OMV.Module.Diagnostics.LogPlugin.Greyhole.superclass.constructor.call(
+					this, initialConfig);
+};
+
+Ext.extend(OMV.Module.Diagnostics.LogPlugin.Greyhole,
+				OMV.Module.Diagnostics.LogPlugin, {
+				});
+OMV.preg("log", "greyhole", OMV.Module.Diagnostics.LogPlugin.Greyhole);
